215f860db4c889f6f0e1f6b89b204c1b0e052d0a
Yash <74345713+yash244466666@users.noreply.github.com>
2023-03-01 05:59:29 +0600

    Add changes as instructed

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-id BIGSERIAL NOT NULL,
+id BIGSERIAL NOT NULL PRIMARY KEY,
 name VARCHAR(50),
 date_of_birth DATE,
 escape_attempts INT,
